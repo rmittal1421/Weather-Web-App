5a553a801d05bedb07bcc8cd4c8d66d48b8c8de7
Raghav Mittal <rmittal@sfu.ca>
2019-04-03 19:16:10 -0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener ('submit', (e) => {
     document.querySelector('#message1').textContent = 'Loading...'
     document.querySelector('#message2').textContent = ''
 
-    fetch (`http://localhost:3000/weather?address=${location}`).then ((response) => {
+    fetch (`/weather?address=${location}`).then ((response) => {
         response.json().then ((data) => {
             if (data.error) document.querySelector('#message1').textContent = data.error
             else {
